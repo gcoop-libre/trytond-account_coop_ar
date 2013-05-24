9e44419d303c6314ad8ae79a377625d2f1aecf2b
Joaquín Sorianello <joac@gcoop.coop>
2013-05-24 17:10:40 -0300

    Add csv based XML generator

--- conversor.py
+++ conversor.py
@@ -1,5 +1,18 @@
+# -*- coding: utf8 -*-
+
+import csv
 import xml.etree.ElementTree as ET
 
+from unicodedata import normalize
+
+def normalizar_string(unicode_string):
+    u"""Retorna unicode_string normalizado para efectuar una búsqueda.
+
+    >>> normalizar_string(u'Mónica Viñao')
+    'monica vinao'
+
+    """
+    return normalize('NFKD', unicode_string).encode('ASCII', 'ignore').lower()
 
 class XMLElement(object):
     """Abstract Represents a xml element"""
@@ -30,7 +43,6 @@ class Field(XMLElement):
     """Represents a Field of tryton"""
     def __init__(self, name, attrs={}, value=None):
         attrs['name'] = name
-        print value
         super(Field, self).__init__('field', attrs, value)
 
 
@@ -41,18 +53,90 @@ class Record(XMLElement):
         super(Record, self).__init__('record', attrs)
 
     def add_field(self, field):
-        """Ads a new field inside the record"""
+        """Adds a new field inside the record"""
         self.element.append(field.element)
 
 
-if __name__ == '__main__':
-    #TODO Write a test, you lazy programmer
-    doc = Document()
-    a = Record('account.account.type.template', 'ar')
-    a.add_field(
-        Field('name', value='Plan de cuentas argentino para cooperativas')
-        )
-    a.add_field(Field('sequence', {'eval': '10'}))
-    doc.add_record(a)
+class WierdXMLGenerator(object):
+    """Takes a csv and creates a lot of xml tags
+    as java programmers like"""
+    types = {
+            'vista': 'view',
+            'otro': 'other',
+            'a pagar': 'payable',
+            'a cobrar': 'receivable',
+            'ingresos': 'revenue',
+            'existencias': 'stock',
+            'gastos':'expense',
+            }
 
-    print ET.dump(doc.tree)
+    def __init__(self, filename):
+        self.filename = filename
+        self.document = Document()
+        self.parents = {}
+        self.record_ids = set()
+
+    def inflate(self):
+        """Make data grow up, because java
+        programmers dont worry about storage"""
+        with open(self.filename, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                self.process_row(row)
+
+        return self.document.tree
+
+    def process_row(self, row):
+        #FIXME unicode
+        group = row['GRUPO']
+        code = row['NUMERO']  # si tiene hijos, ponerle un *
+        kind = self.types.get(row['clase'], '')
+        print row
+        description = row['DESCRIPCION'].decode('utf8')
+        #FIXME ids con caracteres no ascii
+
+        id = normalizar_string(description).replace(' ', '_')
+
+        if id in self.record_ids:
+            raise ValueError('You have defined this id twice motherfucker %s' % id)
+
+        self.record_ids.add(id)
+
+        parent = self.get_parent_for(group, id)
+        record = Record('account.account.template', id)
+        record.add_field(Field('name', value=description))
+        record.add_field(Field('code', value=code))
+        if row['aplazar']:
+            record.add_field(Field('deferral', {'eval': 'True'}))
+
+        if row['conciliar']:
+            record.add_field(Field('reconcile', {'eval': 'True'}))
+
+        record.add_field(Field('kind', value=kind))
+        # Ver como manejar el valor de type
+        #record.add_field(Field('type', value=code))
+
+        record.add_field(Field('parent', {'ref': parent}))
+
+        self.document.add_record(record)
+
+    def get_parent_for(self, group, id):
+        """Return parent of a given group"""
+        self.parents[group] = id
+        print group
+        if '.' in group:
+            key = '.'.join(group.split('.')[:-1])
+            return self.parents[key]
+        else:
+            return 'root'
+
+
+if __name__ == '__main__':
+    import xml.dom.minidom
+
+    def imprimir_lindo(xml_string):
+        return xml.dom.minidom.parseString(xml_string).toprettyxml(encoding='utf8')
+
+    FILENAME = 'cuentas.csv'
+    g = WierdXMLGenerator(FILENAME)
+    print imprimir_lindo(ET.tostring(g.inflate()._root))
