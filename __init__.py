4b30897f2bb20f069617151e8c82f137b35d4a93
Luciano Rossi <lukio@gcoop.coop>
2017-10-27 17:08:39 -0300

    fixes #8 Add wizard to print chart of accounts

--- __init__.py
+++ __init__.py
@@ -1,3 +1,18 @@
-#This file is part of the account_ar module for Tryton.
+#This file is part of the account_coop_ar module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
+
+from trytond.pool import Pool
+from . import account
+
+
+def register():
+    Pool.register(
+        account.PrintChartAccountStart,
+        module='account_coop_ar', type_='model')
+    Pool.register(
+        account.PrintChartAccount,
+        module='account_coop_ar', type_='wizard')
+    Pool.register(
+        account.ChartAccount,
+        module='account_coop_ar', type_='report')
