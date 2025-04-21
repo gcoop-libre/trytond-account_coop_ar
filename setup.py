231f6cc9086f36cf44dd4cae9bc950b89889a15e
Luciano Rossi <lukio@gcoop.coop>
2025-04-21 07:09:52 -0300

    Prepare release 7.0

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ import io
 import os
 import re
 from configparser import ConfigParser
+
 from setuptools import find_packages, setup
 
 MODULE = 'account_coop_ar'
@@ -27,12 +28,13 @@ def read(fname):
 
 def get_require_version(name):
     if name in LINKS:
-        return ''  # '%s @ %s' % (name, LINKS[name])
+        return '%s @ %s' % (name, LINKS[name])
     if minor_version % 2:
         require = '%s >= %s.%s.dev0, < %s.%s'
     else:
         require = '%s >= %s.%s, < %s.%s'
-    require %= (name, major_version, minor_version,
+    require %= (
+        name, major_version, minor_version,
         major_version, minor_version + 1)
     return require
 
@@ -54,9 +56,9 @@ download_url = 'https://github.com/gcoop-libre/trytond-%s/tree/%s.%s' % (
 
 LINKS = {
     'trytonar_account_ar': ('git+https://github.com/tryton-ar/'
-        'account_ar.git@%s.%s#egg=trytonar_account_ar-%s.%s' %
+        'account_ar.git@%s.%s#egg=trytonar-account-ar-%s.%s' %
         (major_version, minor_version, major_version, minor_version)),
-    }
+}
 
 requires = []
 for dep in info.get('depends', []):
@@ -67,14 +69,11 @@ for dep in info.get('depends', []):
 requires.append(get_require_version('trytond'))
 
 tests_require = [get_require_version('proteus')]
-dependency_links = list(LINKS.values())
-if minor_version % 2:
-    dependency_links.append('https://trydevpi.tryton.org/')
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
     description='Tryton module to add an account chart template for Argentina',
-    long_description=read('README'),
+    long_description=read('README.rst'),
     author='gcoop-libre',
     url=url,
     download_url=download_url,
@@ -106,22 +105,21 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         'Natural Language :: Spanish',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Office/Business',
         'Topic :: Office/Business :: Financial :: Accounting',
         ],
     license='GPL-3',
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=requires,
     extras_require={
         'test': tests_require,
         },
-    dependency_links=dependency_links,
     zip_safe=False,
     entry_points="""
     [trytond.modules]
